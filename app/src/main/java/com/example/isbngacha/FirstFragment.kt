0dd73dc1d72844718dab7094ce60750b4ef4c5e5
naga-naga <kiyoharu.639721@gmail.com>
2023-02-25 02:23:22 +0900

    レイアウトを作成

--- app/src/main/java/com/example/isbngacha/FirstFragment.kt
+++ app/src/main/java/com/example/isbngacha/FirstFragment.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.isbngacha.databinding.FragmentFirstBinding
+import com.example.isbngacha.databinding.FragmentIsbnListBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentIsbnListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentIsbnListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.isbnGeneratorButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
--- app/src/main/java/com/example/isbngacha/SecondFragment.kt
+++ app/src/main/java/com/example/isbngacha/SecondFragment.kt
@@ -5,15 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.isbngacha.databinding.FragmentSecondBinding
+import com.example.isbngacha.databinding.FragmentBookDetailBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentBookDetailBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,17 +23,13 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentBookDetailBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 
     override fun onDestroyView() {
